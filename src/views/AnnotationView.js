1dc630cf4a9484b470ef0c33f4898aa26d3558e9
Lennie132 <lennart.v@live.nl>
2016-12-19 22:17:28 +0100

    fixed problem with changing location of dot and problem when changing color, added possibility to change title and description

--- src/views/AnnotationView.js
+++ src/views/AnnotationView.js
@@ -6,6 +6,11 @@ import $ from 'jquery';
 import _ from 'underscore';
 import {View} from 'backbone';
 
+/**
+ * This model is for the annotations. A annotation is shown as a dot which you can drag around. You can click on an
+ * annotation to open a popup. There you can change the title, description and color. You can also delete the annotation
+ * and close the popup.
+ */
 export default class AnnotationView extends View {
     initialize() {
         this.model.on("change", () => this.render(), this);
@@ -28,10 +33,12 @@ export default class AnnotationView extends View {
             "click .annotation__close-details": "closeDetails",
             "click .annotation__delete": "delete",
             "click .annotation__color": "changeColor",
-            "mousedown": "moveStart",
-            "mouseup": "moveStop",
-            "mouseout": "moveStop",
-            "mousemove": "moveElement"
+            "change .annotation__title": "changeTitle",
+            "change .annotation__description": "changeDescription",
+            "mousedown  .annotation__dot": "moveStart",
+            "mouseup  .annotation__dot": "moveStop",
+            "mouseout  .annotation__dot": "moveStop",
+            "mousemove  .annotation__dot": "moveElement"
         };
     }
 
@@ -48,10 +55,29 @@ export default class AnnotationView extends View {
         this.remove();
     }
 
+    /**
+     * If mouse is down register this for moving functions
+     */
     moveStart() {
         this.mousedown = true;
     }
 
+    /**
+     * If the mouse is down and moving, then change te position to the position of the mouse
+     * @param event
+     */
+    moveElement(event) {
+        if (this.mousedown) {
+            let position = this.getPosition(event);
+            this.setPosition(position.x, position.y);
+            this.positionchanged = true;
+        }
+    }
+
+    /**
+     * If the mouse is up again and the position of the dot has changed, then save the new location
+     * @param event
+     */
     moveStop(event) {
         this.mousedown = false;
         if (this.positionchanged) {
@@ -61,23 +87,49 @@ export default class AnnotationView extends View {
         }
     }
 
-    moveElement(event) {
-        if (this.mousedown) {
-            let position = this.getPosition(event);
-            this.setPosition(position.x, position.y);
-            this.positionchanged = true;
-        }
-    }
-
+    /**
+     * If a new color is chosen, delete the class of the dot with the current color and
+     * replace it with the new color class
+     */
     setColor() {
+        this.$el.removeClass((index, css) => {
+            return (css.match(/\bannotation--\S+/g) || []).join(' ');
+        });
         this.$el.addClass("annotation--" + this.model.get('color'));
     }
 
+    /**
+     * Save new chosen color
+     * @param event
+     */
     changeColor(event) {
         let color = $(event.target).data('color');
         this.model.save({color: color});
     }
 
+    /**
+     * Save new chosen title
+     * @param event
+     */
+    changeTitle(event) {
+        let title = $(event.target).val();
+        this.model.save({title: title});
+    }
+
+    /**
+     * Save new chosen description
+     * @param event
+     */
+    changeDescription(event) {
+        let description = $(event.target).val();
+        this.model.save({description: description});
+    }
+
+
+    /**
+     * Sets the size on the canvas
+     * @param size
+     */
     setSize(size) {
         this.$(".annotation__dot").css({
             width: size + 'px',
@@ -102,30 +154,47 @@ export default class AnnotationView extends View {
     }
 
     /**
-     * Gives the position of the annotation relative to the canvas
+     * Gives the position of the annotation relative to the canvas and checks if it is not outside the canvas
      * @param event
      * @returns {{x: number, y: number}}
      */
     getPosition(event) {
         const canvas = $("#canvas");
+        let x, y;
 
         // relative position in pixels
         let relativeX = event.clientX - canvas.offset().left - this.$el.width() / 2;
         let relativeY = event.clientY - canvas.offset().top - this.$el.height() / 2;
 
-        // relative position in procenten
-        let x = (relativeX / canvas.width() * 100);
-        let y = (relativeY / canvas.height() * 100);
+
+        // relative position in percentage with check if the position is in the canvas
+        if (relativeX > 0 && relativeX < (canvas.width() - this.$el.width())) {
+            x = relativeX / canvas.width() * 100;
+        } else {
+            x = Number(this.$el.css('left').replace(/[^-\d\.]/g, '')) / canvas.width() * 100;
+        }
+
+        if (relativeY > 0 && relativeY < (canvas.height() - this.$el.height())) {
+            y = relativeY / canvas.height() * 100;
+        } else {
+            y = Number(this.$el.css('top').replace(/[^-\d\.]/g, '')) / canvas.height() * 100;
+        }
 
         return {x: x, y: y};
     }
 
+    /**
+     * Open detail popup
+     */
     openDetails() {
-        $('.annotation__details').removeClass("annotation__details--visible");
-        this.$('.annotation__details').addClass("annotation__details--visible");
+        $(".annotation__details").removeClass("annotation__details--visible");
+        this.$(".annotation__details").addClass("annotation__details--visible");
     }
 
+    /**
+     * Close detail popup
+     */
     closeDetails() {
-        this.$('.annotation__details').removeClass("annotation__details--visible");
+        this.$(".annotation__details").removeClass("annotation__details--visible");
     }
 }
